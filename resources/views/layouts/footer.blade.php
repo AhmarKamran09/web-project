88d9a47d1da4ab31b0f732c38e4af9e8c40c5388
Zainab Athar <118754216+ZainabAthar@users.noreply.github.com>
2024-12-29 19:49:13 +0500

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -16,29 +16,11 @@
                             <strong>Phone: </strong>+212 682860421
                         </p>
                         <p class="wow fadeIn animated">
-                            <strong>Email: </strong>yusufisawi@gmail.com
+                            <strong>Email: </strong>test@gmail.com
                         </p>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-3">
-                    <h5 class="widget-title wow fadeIn animated">About</h5>
-                    <ul class="footer-list wow fadeIn animated mb-sm-5 mb-md-0">
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Delivery Information</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Terms &amp; Conditions</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-2  col-md-3">
-                    <h5 class="widget-title wow fadeIn animated">My Account</h5>
-                    <ul class="footer-list wow fadeIn animated">
-                        <li><a href="my-account.html">My Account</a></li>
-                        <li><a href="#">View Cart</a></li>
-                        <li><a href="#">Track My Order</a></li>
-                        <li><a href="#">Order</a></li>
-                    </ul>
-                </div>
+                
 
             </div>
         </div>
